470337d4d71e93efe0cd5b4911601409f4f06522
Caio Dias <131923914+CdBr4zil@users.noreply.github.com>
2025-07-16 17:16:35 -0300

    Update registro_ra.py

--- Arquivos/utils/registro_ra.py
+++ Arquivos/utils/registro_ra.py
@@ -1,6 +1,7 @@
 import random
 
 ra_usados = set()
+id_usados = set()
 
 def gerar_ra():
     while True:
@@ -9,3 +10,11 @@ def gerar_ra():
         if ra not in ra_usados:
             ra_usados.add(ra)
             return ra
+        
+def gerar_id():
+    while True:
+        id = random.randint(100000, 999999)
+        
+        if id not in id_usados:
+            id_usados.add(id)
+            return id
